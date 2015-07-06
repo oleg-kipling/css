631848fd07313b29578f8278edf269f5615a425d
Oleg Sugatov <oleg.kipling@gmail.com>
2015-07-06 12:04:14 +0300

    hover_animation

---
+++ Materialize/js/nav-collapse.js
@@ -0,0 +1,22 @@
+(function($){
+    
+    $(function(){
+
+    $('.button-collapse').sideNav();
+
+    });// end of document ready
+    
+    $(function(){
+
+    $('.scrollspy').scrollSpy();
+
+    });// end of document ready
+    
+
+    $(function(){
+        
+        $('.parallax').parallax();
+        
+    });// end of document ready
+    
+})(jQuery); // end of jQuery name space
